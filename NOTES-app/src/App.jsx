1daae80c646eba7cb14d41094d18c96adf9b71be
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-06 11:20:59 +0530

    #Notes-app @01

--- NOTES-app/src/App.jsx
+++ NOTES-app/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className=" bg-black h-screen">
+      <form className="flex items-start   p-10 " action="">
+        <div className="flex w-1/2 flex-col gap-3 items-start relative ">
+          <input
+            className="text-white px-4 h-20  w-full border-2 outline-none rounded font-medium"
+            type="text"
+            placeholder="Enter your Note Heading"
+          />
+          <textarea
+            className=" h-40  px-2 w-full  text-white border-2 outline-none rounded font-medium"
+            placeholder="Writing your Details"
+            type="text"
+          />
+          <button className="bg-white w-full px-5 py-7 rounded outline-none text-black font-medium ">
+            Add Note
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default App
+export default App;
