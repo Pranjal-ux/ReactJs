b627c29a9fd9eea7e30257a1d8a675ce228aaba8
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-07 19:21:10 +0530

    #UseEffectDone

--- UseEffect/src/App.jsx
+++ UseEffect/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [num2, setNum2] = useState(100);
+  useEffect(() => {
+    console.log("use effect is running....");
+  }, [num]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Value of num is{num}</h1>
+      <h2>Value of user is{num2}</h2>
+      <button
+        onMouseEnter={() => {
+          setNum(num + 1);
+        }}
+        onMouseLeave={() => {
+          setNum2(num2 + 5);
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
