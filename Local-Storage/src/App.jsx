2126516130a5bf9c329a7c82b10c145d379525cf
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-07 10:08:05 +0530

    #DONE localStorage

--- Local-Storage/src/App.jsx
+++ Local-Storage/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const StoredValue = localStorage.getItem("user");
+  console.log(StoredValue);
+  localStorage.setItem("age", "20");
+  const age = localStorage.getItem("age");
+  console.log(age);
+  localStorage.removeItem("user");
+  localStorage.clear();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  const user = {
+    username: "pranjal",
+    age: 20,
+    city: "Lucknow",
+  };
+  localStorage.setItem("user", JSON.stringify(user));//THIS IS USE TO CONVERT OBJECT TO STRING 
+  console.log(user);
+  const user1 = JSON.Parse(localStorage.getItem("user"));//AND IT IS USE TO CONVERT STRING TO OBJECT
+  console.log(user1);
+  return <div>App</div>;
+};
 
-export default App
+export default App;
