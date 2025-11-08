0bce11b05043fb8c7f5bebe7efde8aa0367a471a
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-08 20:17:43 +0530

    Done Gallery project

--- Gallery-Project/src/App.jsx
+++ Gallery-Project/src/App.jsx
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useEffect } from "react";
 const App = () => {
   const [userData, setUserData] = useState([]);
+  const [index, setIndex] = useState(0);
   const getData = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=3&limit=32"
+      `https://picsum.photos/v2/list?page=${index}&limit=15`
     );
     setUserData(response.data);
-    console.log("Data mil gaya");
   };
+  useEffect(() => {
+    getData();
+  }, [index]);
   let printUserData = "No Data is found ";
   if (userData.length > 0) {
     printUserData = userData.map((e, idx) => {
       return (
         <div key={idx}>
-          <div className="h-40 w-44 rounded-xl overflow-hidden ">
-            <img
-              className="h-full w-full object-cover  "
-              src={e.download_url}
-              alt="image not found!!"
-            />
-          </div>
-          <h2 className="font-bold text-xl text-white mt-2">{e.author}</h2>
+          <a href={e.url} target="_blank">
+            <div className="h-40 w-44 rounded-xl overflow-hidden mt-3 ">
+              <img
+                className="h-full w-full object-cover  "
+                src={e.download_url}
+                alt="image not found!!"
+              />
+            </div>
+
+            <h2 className="font-bold text-lg text-white mt-1">{e.author}</h2>
+          </a>
         </div>
       );
     });
@@ -29,13 +36,28 @@ const App = () => {
 
   return (
     <div className="h-screen bg-black overflow-auto text-whites ">
-      <button
-        onClick={getData}
-        className="bg-teal-800 px-10 py-5 rounded m-3 w-[200px] text-white"
-      >
-        Get data
-      </button>
+      <h1 className="bg-red fixed text-5xl">{index}</h1>
       <div className="text-white flex flex-wrap  gap-12  ">{printUserData}</div>
+      <div className="flex justify-center items-center gap-3 mb-3 ">
+        <button
+          onClick={() => {
+            if (index > 1) setIndex(index - 1);
+            setUserData([]);
+          }}
+          className="bg-yellow-500 px-7 py-2 text-sm active:scale-20 rounded-lg "
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => {
+            setIndex(index + 1);
+            setUserData([]);
+          }}
+          className="bg-yellow-500 px-7 py-2 text-sm active:scale-20 rounded-lg "
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
