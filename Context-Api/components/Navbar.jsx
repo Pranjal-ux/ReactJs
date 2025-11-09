4503f6af7c75008b6b748c855063d05e530290d1
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-09 19:22:48 +0530

    #Last step

---
+++ Context-Api/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav2 from "../components/Nav2";
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <h1>ContextApi</h1>
+      <Nav2 />
+    </div>
+  );
+};
+
+export default Navbar;
