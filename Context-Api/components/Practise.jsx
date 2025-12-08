1ea78b1055cde6113e48fbcb72857832ac4082ab
Pranjal-ux <pranjal2shukla@gmail.com>
2025-12-08 11:39:41 +0530

    changes

---
+++ Context-Api/components/Practise.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Practise = () => {
+  return (
+    <div>
+      <h1 className="text-white">This context Api</h1>
+    </div>
+  );
+};
+
+export default Practise;
---
+++ src/components/AuthenticationPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { LoginForm } from "./login-form";
+
+const AuthenticationPage = () => {
+  return (
+    <div>
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
